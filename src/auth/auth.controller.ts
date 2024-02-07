acdce3d8120b88396f1c483397da84aba85f3468
Patrick Van Weelden <pvanweel@yahoo.com>
2024-02-07 15:23:05 -0600

    added GqlAuthGuard and strategies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Post, Res, UseGuards } from '@nestjs/common';
-import { localAuthGuard } from './guards/local-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 import { CurrentUser } from './current-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
   @Post('login')
-  @UseGuards(localAuthGuard)
+  @UseGuards(LocalAuthGuard)
   async login(
     @CurrentUser() user: User,
     @Res({ passthrough: true }) response: Response,
